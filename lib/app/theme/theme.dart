67e3d4cc0cbd49841b3c5709d9a747b1ce84fcaf
Kirill <kirillbar24@gmail.com>
2024-04-22 15:28:00 +0400

    fix colorScheme

--- lib/app/theme/theme.dart
+++ lib/app/theme/theme.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 final theme = ThemeData(
   scaffoldBackgroundColor: const Color.fromRGBO(242, 242, 242, 1),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
 );
